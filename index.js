0e05bbba272b33dc0d5fd02a3da42b042552a80d
Arjun <arjunrvk2502@gmail.com>
2026-01-16 00:21:07 +0530

    works

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Everyone, this is a  node application inside a Docker container on google compute engine \n CICD is working\n Aravind and co ku oru tutorial kettuvum ajsdjfkjdfjfseifheiu djfwjnfijwnf \n');
+  res.end('Hello Everyone, this is a  node application inside a Docker container on google compute engine \n CICD is working\n Aravind and co ku oru tutorial it works!!!! \n');
 });
 
 server.listen(port, hostname, () => {
